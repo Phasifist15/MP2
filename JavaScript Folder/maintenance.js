f1a4f92b05910b7a4d4c63155efa34f4e978583d
Phasifist15 <hyrumlaudato15@gmail.com>
2024-01-18 09:54:11 +0800

    new file:   CSS Folder/custom.css 	new file:   CSS Folder/maintenance.css 	modified:   CSS Folder/style.css 	new file:   JavaScript Folder/maintenance.js 	modified:   custom.html 	modified:   hardware.html 	modified:   index.html 	modified:   maintenance.html

---
+++ JavaScript Folder/maintenance.js
@@ -0,0 +1,47 @@
+// LOGIN MODAL
+
+  // Hide/Show Password
+
+    // Login Modal
+
+    const togglePassword = document.getElementById('togglePassword');
+    const password = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function() {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    toggleIcon.className = type === 'password' ? 'bi bi-eye' : 'bi bi-eye-slash';
+    });
+
+
+  // Sign Up Modal
+
+      // Sign Up inside Log In modal
+
+      const signupBtn = document.getElementById('signupBtn');
+      const signupAnchored = document.getElementById('signupAnchored');
+
+      signupAnchored.addEventListener('click', function(event){
+
+        event.preventDefault();
+
+        const loginModalCloseButton = new bootstrap.Modal(document.querySelector('#login .btn-close'));
+        loginModalCloseButton.hide();
+
+        signupBtn.click();
+      });
+
+      // Log In inside Sign Up modal
+
+      const loginAnchored = document.getElementById('loginAnchored');
+      const loginBtn = document.getElementById('loginBtn');
+
+      loginAnchored.addEventListener('click', function(event){
+
+        event.preventDefault();
+
+        const signupModalCloseButton = new bootstrap.Modal(document.querySelector('#signupModal .btn-close'));
+        signupModalCloseButton.hide();
+
+        loginBtn.click();
+      });
