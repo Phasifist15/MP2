301b5b6f1832515b7e0c12a138ae78549ab508f0
Phasifist15 <hyrumlaudato15@gmail.com>
2024-01-18 16:09:39 +0800

    new file:   JavaScript Folder/footer.js 	modified:   about.html 	modified:   backup.html 	modified:   custom.html 	modified:   faq.html 	modified:   hardware.html 	modified:   index.html 	modified:   maintenance.html 	modified:   security.html 	modified:   services.html

---
+++ JavaScript Folder/footer.js
@@ -0,0 +1,11 @@
+function aboutUs() {
+  window.location.href = 'about.html';
+}
+
+function services() {
+  window.location.href = 'services.html';
+}
+
+function faq() {
+  window.location.href = 'faq.html';
+}
